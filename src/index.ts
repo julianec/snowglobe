5d0535cbf1c6646ae4a124f72e11efb556c903fd
Juliane Clausen <julianeclausen@MacBook-Air.fritz.box>
2022-01-04 00:21:09 +0100

    commented on some stuff on index.ts to better understand it

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,19 @@ bootstrapExtra().catch(e => console.error(e));
 
 let currentPopup: any = undefined;
 const today = new Date();
+// define a string with the date format 'HH:MM'
 const time = today.getHours() + ":" + today.getMinutes();
 
+// open popup with current time
+// no idea what clockPopup refers to
 WA.room.onEnterZone('clock', () => {
     currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
 })
 
+// close popup when leaving zone 'clock'
 WA.room.onLeaveZone('clock', closePopUp)
 
+// close popup and set currentPopup to undefined
 function closePopUp(){
     if (currentPopup !== undefined) {
         currentPopup.close();
